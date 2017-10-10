e55b7710442944c0548bd54184bb8ad48d9352b7
朱文杰； <zwj.zhu@tianyitechs.com>
2017-10-10 14:12:27 +0800

    自定义NavigationLayout实现导航栏点击放大效果

--- app/src/main/java/originfx/originecn/cn/mvplibrarydemo/MainActivity.java
+++ app/src/main/java/originfx/originecn/cn/mvplibrarydemo/MainActivity.java
@@ -1,72 +1,47 @@
 package originfx.originecn.cn.mvplibrarydemo;
 
 import android.animation.ValueAnimator;
-import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import originfx.originecn.cn.mvplibrary.BaseActivity;
+import originfx.originecn.cn.navigationlayout.NavigationLayout;
 
-public class MainActivity extends BaseActivity<ToastIbseView, ToastIBasePresenter> implements ToastIbseView, View.OnClickListener {
+public class MainActivity extends BaseActivity<ToastIbseView, ToastIBasePresenter> implements ToastIbseView {
     ImageView img1, img2, img3, img4, img5;
     public List<ImageView> list_img = new ArrayList<>();
     ValueAnimator mAnimator;
     public int viewId;
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img1 = (ImageView) findViewById(R.id.img1);
-        img2 = (ImageView) findViewById(R.id.img2);
-        img3 = (ImageView) findViewById(R.id.img3);
-        img4 = (ImageView) findViewById(R.id.img4);
-        img5 = (ImageView) findViewById(R.id.img5);
-        img1.setOnClickListener(this);
-        img2.setOnClickListener(this);
-        img3.setOnClickListener(this);
-        img4.setOnClickListener(this);
-        img5.setOnClickListener(this);
-        list_img.add(img1);
-        list_img.add(img2);
-        list_img.add(img3);
-        list_img.add(img4);
-        list_img.add(img5);
+        NavigationLayout Navigation = (NavigationLayout) findViewById(R.id.NavigationLayout);
+        ArrayList<Bitmap> bitmaps = new ArrayList<>();
+        Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.mipmap.img1);
+        Bitmap bitmap2 = BitmapFactory.decodeResource(getResources(), R.mipmap.img2);
+        Bitmap bitmap3 = BitmapFactory.decodeResource(getResources(), R.mipmap.img3);
+        Bitmap bitmap4 = BitmapFactory.decodeResource(getResources(), R.mipmap.img4);
+        Bitmap bitmap5 = BitmapFactory.decodeResource(getResources(), R.mipmap.img5);
+        bitmaps.add(bitmap1);
+        bitmaps.add(bitmap2);
+        bitmaps.add(bitmap3);
+        bitmaps.add(bitmap4);
+        bitmaps.add(bitmap5);
+        Navigation.setListImageView(bitmaps, this);
 
 
         presenter.ToastMSG();
     }
 
-    private void setImageSize(final ImageView image) {
-        mAnimator = ValueAnimator.ofInt(50, 100);
-        //2.为目标对象的属性变化设置监听器
-        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                // 3.为目标对象的属性设置计算好的属性值
-                int animatorValue = (int) animation.getAnimatedValue();
-                ViewGroup.LayoutParams layoutParams = image.getLayoutParams();
-                layoutParams.height = dip2px(MainActivity.this, animatorValue);
-                image.setLayoutParams(layoutParams);
-            }
-        });
-        //4.设置动画的持续时间、是否重复及重复次数等属性
-        mAnimator.setDuration(200);
-        mAnimator.setRepeatCount(0);
-        mAnimator.setRepeatMode(ValueAnimator.REVERSE);
-        //5.为ValueAnimator设置目标对象并开始执行动画
-        mAnimator.setTarget(image);
-        mAnimator.start();
-
-
-    }
 
     @Override
     public ToastIBasePresenter initBasePresenter() {
@@ -78,36 +53,5 @@ public class MainActivity extends BaseActivity<ToastIbseView, ToastIBasePresente
         showToast(msg);
     }
 
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    public static int dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
-    }
 
-    /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     */
-    public static int px2dip(Context context, float pxValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (pxValue / scale + 0.5f);
-    }
-
-    @Override
-    public void onClick(View view) {
-        for (int i = 0; i < list_img.size(); i++) {
-
-            if (view.getId() == list_img.get(i).getId()) {
-                if (viewId != view.getId())
-                    setImageSize(list_img.get(i));
-                viewId = view.getId();
-            } else {
-                ViewGroup.LayoutParams layoutParams = list_img.get(i).getLayoutParams();
-                layoutParams.height = dip2px(MainActivity.this, 48);
-                list_img.get(i).setLayoutParams(layoutParams);
-            }
-        }
-
-    }
 }
---
+++ navigationlayout/src/main/java/originfx/originecn/cn/navigationlayout/NavigationLayout.java
@@ -0,0 +1,113 @@
+package originfx.originecn.cn.navigationlayout;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/10.
+ */
+
+public class NavigationLayout extends LinearLayout implements View.OnClickListener {
+    Context context;
+    ValueAnimator mAnimator;
+    public List<ImageView> list_img = new ArrayList<>();
+    public int viewId;
+
+    public NavigationLayout(Context context) {
+        super(context);
+    }
+
+    public NavigationLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.BOTTOM);
+        setClipChildren(false);
+    }
+
+    public NavigationLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * @param list_bitmap
+     * @param context
+     */
+    public void setListImageView(List<Bitmap> list_bitmap, Context context) {
+        this.context = context;
+        for (int i = 0; i < list_bitmap.size(); i++) {
+            ImageView imageView = new ImageView(context);
+            imageView.setId(i);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setImageBitmap(list_bitmap.get(i));
+            imageView.setOnClickListener(this);
+            addView(imageView);
+            list_img.add(imageView);
+            if (i == (list_bitmap.size() - 1) / 2) {
+                ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+                layoutParams.height = dip2px(context, 100);
+                imageView.setLayoutParams(layoutParams);
+            }
+        }
+        postInvalidate();
+    }
+
+    @Override
+    public void onClick(View view) {
+        for (int i = 0; i < list_img.size(); i++) {
+            if (view.getId() == list_img.get(i).getId()) {
+                if (viewId != view.getId())
+                    setImageSize(list_img.get(i));
+                viewId = view.getId();
+            } else {
+                ViewGroup.LayoutParams layoutParams = list_img.get(i).getLayoutParams();
+                layoutParams.height = dip2px(context, 48);
+                list_img.get(i).setLayoutParams(layoutParams);
+            }
+        }
+    }
+
+    private void setImageSize(final ImageView image) {
+        mAnimator = ValueAnimator.ofInt(50, 100);
+        //2.为目标对象的属性变化设置监听器
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // 3.为目标对象的属性设置计算好的属性值
+                int animatorValue = (int) animation.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = image.getLayoutParams();
+                layoutParams.height = dip2px(context, animatorValue);
+                image.setLayoutParams(layoutParams);
+            }
+        });
+        //4.设置动画的持续时间、是否重复及重复次数等属性
+        mAnimator.setDuration(200);
+        mAnimator.setRepeatCount(0);
+        mAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        //5.为ValueAnimator设置目标对象并开始执行动画
+        mAnimator.setTarget(image);
+        mAnimator.start();
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+}
